ac639466e232f7e96ca8b2d2ede49ef3d3b2e4db
reduno96 <reduno96@gmail.com>
2023-01-31 13:31:42 +0100

    Modification Scroll

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+let el = document.querySelector(".scroller");
+let height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+  let scrollTop = document.documentElement.scrollTop;
+  el.style.width = `${(scrollTop / height) * 100}%`;
+});
